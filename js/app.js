c152895fae8681433f00c81b8dae8f8d583be984
Nicholas D'Amico <nickalan82@icloud.com>
2017-02-16 17:01:07 -0500

    removed un-needed function

--- js/app.js
+++ js/app.js
@@ -1395,21 +1395,24 @@ function a(b,d){function e(a,b){return function(){return a.apply(b,arguments)}}v
   }
 }(jQuery, window, window.document));
 
+//	Get all image grid anchor elements
 var $anchors = $('div.columns > a');
 
-$anchors.each( function () {
-	var $this = $(this);
-	var data = $this.attr('data-reveal-id');
-});
-
+//	Add on 'click' event to anchor elements
 $anchors.on('click', function (e) {	
+	//	Remove any image loaded into modal on click.
+		// This stops from multiple images showing up in modal display.
 	$('img.on').remove();
 	var parent = $(e.target).parent();
+	//	Get relative img path for photo, url path stored in custom attr data-img-url.
 	var src = parent.attr('data-img-url');
+	//	Get modal id for cliked element stored in data-reaveal-id attr.
 	var divId = parent.attr('data-reveal-id');
+	//	jQuery Object from divId, used to select modal.
 	var $modalTitle = $('#' + divId + '> h2');
+	//	Create <img> element
 	$newItem = $('<img class="on" src="' + src + '" />');
-	
+	//	Append <img> element to selected modal for display of full size photo.
 	$modalTitle.after($newItem);
 	
 });
